a14b7cf98f5086d3bae18acb4ad44bfa0ff3c1f6
Cindy Le <36822198+DotCindy@users.noreply.github.com>
2018-07-24 12:38:47 -0400

    Update Lab3.sql

--- Lab3.sql
+++ Lab3.sql
@@ -8,11 +8,3 @@ SELECT  LAST_NAME "Last Name", SALARY "Salary", SALARY*1.05 "Salary+5%",
 ROUND(((SALARY*1.05)-SALARY)*12) "Cost of Pay Increase"
 FROM    EMPLOYEES
 WHERE   DEPARTMENT_ID IN(20, 50, 70);
-
-/* 2) Write a query to display the tomorrow’s date in the following format:
-September 28th of year 2006  <- this is the format for the date you display. Your result will 
-depend on the day when you create this query.
-Label the column Tomorrow. */
-
-SELECT TO_CHAR (SYSDATE+1, 'Mon DD"th of year" YYYY') "Tomorrow"
-FROM DUAL;
